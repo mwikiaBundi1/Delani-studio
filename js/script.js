70199322ea581f26b3cc464b6e959fbb6d042cf6
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-02-24 07:55:22 +0300

    enable user input data in contact us section

--- js/script.js
+++ js/script.js
@@ -8,4 +8,18 @@ $(document).ready(function () {
     $(".product_icon").click(function () {
         $("#pmanagent").toggle();
     });
+    $("#submit").click(function(){
+        event.preventDefault()
+        var name=$("#name").val();
+        var email=$("#email").val();
+        var message=$("#message").val();
+        if(name==""|| email=="" || message==""){
+            $("#results").html("please fill all fields in this form");
+            $("#results").css('background-color', 'red')
+        } else{
+            $("#results").html(name + " we have received your message. thank you for reaching out to us");
+            $("#results").css('background-color', 'green')
+            $("form")[0].reset(); 
+             }
+    });
 });
