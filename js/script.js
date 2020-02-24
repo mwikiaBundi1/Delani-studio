e4365fccb2b5ffd36042455c3f76f463bbf1b5bd
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-02-24 04:10:51 +0300

    what we do section with icons and its descriptions complete

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".design_icon").click(function () {
+        $("#design-p").toggle();
+    });
+    $(".dev_icon").click(function () {
+        $("#development").toggle();
+    });
+    $(".product_icon").click(function () {
+        $("#pmanagent").toggle();
+    });
+});
